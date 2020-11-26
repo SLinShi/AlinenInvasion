7235e3d6d17e50409722cfbf155380ab98099aae
anonymity <anonymity_one@outlook.com>
2020-11-26 13:20:42 +0800

    update bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 
 class AlinenInvasion:
@@ -28,12 +30,12 @@ class AlinenInvasion:
         # 初始化ship
         self.ship = Ship(self)
 
-    def run_game(self):
-        # 开始游戏主循环
-        while True:
-            self._check_events()
-            self.ship.update()
-            self._update_screen()
+        # 初始化子弹编组
+        self.bullets = pygame.sprite.Group()
+
+        # 初始化外星人编组
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()  # 创建外星人
 
     # 监视键盘和鼠标事件
     def _check_events(self):
@@ -56,10 +58,19 @@ class AlinenInvasion:
             self.ship.moving_up = True
         elif event.key == pygame.K_DOWN:
             self.ship.moving_down = True
+        elif event.key == pygame.K_SPACE:
+            # 空格开火
+            if len(self.bullets) < self.settings.bullet_allowed:
+                self._fire_bullet()
 
         # Q快捷键退出
         elif event.key == pygame.K_q:
             sys.exit()
+
+    # 创建一颗子弹加入编组
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     #  响应松开
     def _check_keyup_events(self, event):
@@ -72,14 +83,68 @@ class AlinenInvasion:
         elif event.key == pygame.K_DOWN:
             self.ship.moving_down = False
 
+    # 更新子弹位置并删除消失的子弹
+    def _update_bullets(self):
+        self.bullets.update()  # 每个子弹调用bullet中的update
+
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    # 创建一个外星人并放置在当前行
+    def _create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
+    # 创建外星人群
+    def _create_fleet(self):
+        # 创建一个外星人
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_alien_x = available_space_x // (2 * alien_width)
+
+        # 计算屏幕可容纳多少行外星人
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) -
+                             ship_height)
+        number_rows = available_space_y // (3 * alien_height)
+
+        # 创建外星人群
+        for row_number in range(number_rows):
+            for alien_number in range(number_alien_x):
+                self._create_alien(alien_number, row_number)
+
     # 更新屏幕
     def _update_screen(self):
         # 每次循环重新绘制屏幕
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()  # 绘制飞船
 
+        # 绘制子弹
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        # 调用编组的draw方法
+        self.aliens.draw(self.screen)
+
         # 让最近绘制的屏幕可见
         pygame.display.flip()
+
+    def run_game(self):
+        # 开始游戏主循环
+        while True:
+            self._check_events()
+            self.ship.update()
+            # 更新子弹
+            self._update_bullets()
+            # 更新屏幕
+            self._update_screen()
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -9,3 +9,10 @@ class Settings:
 
         # 飞船速度设置
         self.ship_speed = 1.5
+
+        #  子弹设置
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 100  # 限制子弹最大数目
